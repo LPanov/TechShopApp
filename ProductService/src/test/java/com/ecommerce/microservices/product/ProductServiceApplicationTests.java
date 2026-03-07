e27dc7b5d417f80523c360bdf58626550a0de89e
LPanov <lubomirpanov99@gmail.com>
2026-03-07 21:17:58 +0200

    test: Created Integration Test for createProduct option with TestContainers
    Updated the pom.xml file to include dependencies for TestContainers and MongoDB.
    
    Created a new test class ProductServiceApplicationTests
    
    Added a test method testCreateProduct to test the product creation functionality.
    
    Used TestContainers to spin up a MongoDB instance for testing.
    
    Used Spring's TestRestTemplate to send a POST request to the product creation endpoint and verify the response.

--- ProductService/src/test/java/com/ecommerce/microservices/product/ProductServiceApplicationTests.java
+++ ProductService/src/test/java/com/ecommerce/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,54 @@
 package com.ecommerce.microservices.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mongodb.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+    @ServiceConnection
+    static MongoDBContainer mongoDbContainer = new MongoDBContainer("mongo:7.0.5");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+
+        RestAssured.proxy = null;
+    }
+
     @Test
-    void contextLoads() {
+    void createProduct_happyPath_shouldCreateProduct() {
+        String requestBody = """
+                            {
+                                "name": "Iphone 5S",
+                                "description": "This is a great phone",
+                                "price": 100
+                            }
+                        """;
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("Iphone 5S"))
+                .body("description", Matchers.equalTo("This is a great phone"))
+                .body("price", Matchers.equalTo(100));
     }
 
 }
