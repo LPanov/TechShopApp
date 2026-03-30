b74f79feee5d2d0cb6c29f39b1abf793534fbaae
LPanov <lubomirpanov99@gmail.com>
2026-03-30 21:46:37 +0200

    fix: Finished order-service and tested with Postman

--- order-service/src/main/java/com/ecommerce/microservices/order/web/OrderController.java
+++ order-service/src/main/java/com/ecommerce/microservices/order/web/OrderController.java
@@ -4,13 +4,9 @@ import com.ecommerce.microservices.order.dto.OrderRequest;
 import com.ecommerce.microservices.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
 public class OrderController {
