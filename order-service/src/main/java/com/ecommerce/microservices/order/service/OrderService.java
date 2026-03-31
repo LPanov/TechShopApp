430862e3478d61a1eeb1a6441603b4eb509b7d57
LPanov <lubomirpanov99@gmail.com>
2026-03-31 21:17:50 +0200

    feature: Added feign client to the order service

--- order-service/src/main/java/com/ecommerce/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/ecommerce/microservices/order/service/OrderService.java
@@ -1,23 +1,35 @@
 package com.ecommerce.microservices.order.service;
 
+import com.ecommerce.microservices.order.client.InventoryClient;
 import com.ecommerce.microservices.order.dto.OrderRequest;
 import com.ecommerce.microservices.order.model.Order;
 import com.ecommerce.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = buildOrder(orderRequest);
+        var isProductInStock = inventoryClient.checkInventory(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRepository.save(order);
+        if (isProductInStock) {
+            Order order = buildOrder(orderRequest);
+            orderRepository.save(order);
+
+            log.info("Order placed successfully. Order number: {}", order.getOrderNumber());
+        } else {
+            throw new RuntimeException("Product with sku code:" + orderRequest.skuCode() + " is out of stock");
+        }
+
     }
 
     private static Order buildOrder(OrderRequest orderRequest) {
