6312592dde0c65c1aae5216900e6172c31de2a6e
LPanov <lubomirpanov99@gmail.com>
2026-04-26 13:06:15 +0300

    feat: Add Swagger configuration and update ProductService Spring version
    - Updated Spring Boot version to 3.0.0 in ProductService
    - Added OpenApiConfig class to ProductService for Swagger configuration
    - Updated application.properties in ProductService and in order-service to include Swagger UI path

--- ProductService/src/test/java/com/ecommerce/microservices/product/ProductServiceApplicationTests.java
+++ ProductService/src/test/java/com/ecommerce/microservices/product/ProductServiceApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.mongodb.MongoDBContainer;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
