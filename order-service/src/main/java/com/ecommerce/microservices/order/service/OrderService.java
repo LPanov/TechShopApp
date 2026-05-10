8a3df4e25b7e2d279248364fe8d17c92d50e93d9
LPanov <lubomirpanov99@gmail.com>
2026-05-10 09:39:12 +0300

    feature: Implemented Kafka into order service and created Notification service

--- order-service/src/main/java/com/ecommerce/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/ecommerce/microservices/order/service/OrderService.java
@@ -2,10 +2,12 @@ package com.ecommerce.microservices.order.service;
 
 import com.ecommerce.microservices.order.client.InventoryClient;
 import com.ecommerce.microservices.order.dto.OrderRequest;
+import com.ecommerce.microservices.order.event.OrderPlacedEvent;
 import com.ecommerce.microservices.order.model.Order;
 import com.ecommerce.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -17,6 +19,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.checkInventory(orderRequest.skuCode(), orderRequest.quantity());
@@ -24,6 +27,11 @@ public class OrderService {
         if (isProductInStock) {
             Order order = buildOrder(orderRequest);
             orderRepository.save(order);
+
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to kafka topic order placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to kafka topic order placed", orderPlacedEvent);
 
             log.info("Order placed successfully. Order number: {}", order.getOrderNumber());
         } else {
